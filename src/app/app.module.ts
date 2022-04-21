cd85980a63ed3677b3f5aecdc43063eb63efe345
Lautaro <lautaroivansanmartin@hotmail.com>
2022-04-21 18:16:26 -0300

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent
+    SignInComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
