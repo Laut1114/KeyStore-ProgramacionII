c893e53d40cdedbd8da975d54828b68a624a1d29
lauta <lautaroivansanmartin@hotmail.com>
2022-04-30 01:08:55 -0300

    acomodando el carrousel

--- src/app/pages/home/components/carrousel-home/carrousel-home.component.ts
+++ src/app/pages/home/components/carrousel-home/carrousel-home.component.ts
@@ -11,27 +11,7 @@ export class CarrouselHomeComponent implements OnInit {
 
   productos!: ProductInterface[];
 
-  responsiveOptions;
-
-  constructor(private productService: ProductService) {
-    this.responsiveOptions = [
-      {
-        breakpoint: '1024px',
-        numVisible: 3,
-        numScroll: 3
-      },
-      {
-        breakpoint: '768px',
-        numVisible: 2,
-        numScroll: 2
-      },
-      {
-        breakpoint: '560px',
-        numVisible: 1,
-        numScroll: 1
-      }
-    ];
-  }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(){
     this.productService.getProducts().then(products => {
