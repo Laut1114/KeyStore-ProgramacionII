44ca65c428d6ac01ef90a24b2e452bf72cb7a092
lauta <lautaroivansanmartin@hotmail.com>
2022-05-04 17:39:57 -0300

    commit

--- src/app/pages/home/components/carrousel-home/carrousel-home.component.ts
+++ src/app/pages/home/components/carrousel-home/carrousel-home.component.ts
@@ -10,7 +10,6 @@ import { ProductService } from 'src/app/services/product.service';
 export class CarrouselHomeComponent implements OnInit {
 
   productos: ProductInterface[];
-  responsiveOptions: { breakpoint: string; numVisible: number; numScroll: number; }[];
 
   constructor(private productService: ProductService) {}
 
@@ -18,24 +17,5 @@ export class CarrouselHomeComponent implements OnInit {
     this.productService.getProducts().then(products => {
       this.productos = products;
     });
-
-    this.responsiveOptions = [
-      {
-          breakpoint: '1024px',
-          numVisible: 3,
-          numScroll: 3
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
-
   }
 } 
